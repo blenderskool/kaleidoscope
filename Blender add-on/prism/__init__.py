70e71cb4daf00b875a026405b9dc82f14fe5c30b
blenderskool <akash.hamirwasia@gmail.com>
2016-11-05 15:10:07 +0530

    Added Importing Files Feature

--- Blender add-on/prism/__init__.py
+++ Blender add-on/prism/__init__.py
@@ -12,8 +12,11 @@ bl_info = {
 
 import bpy
 from . import spectrum, intensity
-import nodeitems_utils
+import nodeitems_utils, zipfile, os
+import urllib
+import urllib.request
 from nodeitems_utils import NodeCategory, NodeItem
+from bpy_extras.io_utils import ImportHelper
 
 class PrismCategory(NodeCategory):
     @classmethod
@@ -31,6 +34,55 @@ node_categories = [
         ]),
     ]
 
+class Prism(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    def draw(self, context):
+        layout = self.layout
+        box = layout.box()
+        col = box.column()
+        row = col.row()
+        row.alignment ='CENTER'
+        row.label("INSTALLING FILES")
+        col.label()
+        col.label("1) Download the .zip files which has the files to import. DO NOT EXTRACT IT")
+        col.label("2) Click the 'Import Files' button below, and select the .zip file")
+        col.label("     NOTE: SELECT ONLY 1 ZIP FILE ONCE")
+        col.label("3) Click the 'Install Files' button. The files should be installed")
+        col.label()
+        row = col.row()
+        row.scale_y = 1.2
+        row.label("")
+        row.operator(Prism_Import.bl_idname, text='Import Files', icon='PACKAGE') #Install Presets button
+        row.label("")
+        col.label()
+        if bpy.context.scene.prism_props_import_files == True:
+            col.label("There was a problem in importing the files.", icon='ERROR')
+
+class Prism_Import(bpy.types.Operator, ImportHelper): #Importing Presets
+    """Install .zip file in the add-on"""
+    bl_idname = "enrich.install_presets"
+    bl_label = "Install Files"
+
+    filename_ext = ".zip"
+
+    filter_glob = bpy.props.StringProperty(
+            default="*.zip",
+            options={'HIDDEN'},
+            )
+
+    def execute(self, context):
+        try:
+            zipref = zipfile.ZipFile(self.filepath, 'r')
+            path = os.path.dirname(__file__)
+            if (path != ""):
+                zipref.extractall(path) #Extract to the Enrich add-on Folder
+                zipref.close()
+            bpy.context.scene.prism_props_import_files = False
+        except:
+            bpy.context.scene.prism_props_import_files = True
+        return {'FINISHED'}
+
 def register():
     try:
         bpy.utils.register_module(__name__)
@@ -38,10 +90,12 @@ def register():
         pass
     spectrum.register()
     intensity.register()
+    bpy.types.Scene.prism_props_import_files = bpy.props.BoolProperty(name="Prism Import", description="Checks if the zip file is properly imported", default=False)
     nodeitems_utils.register_node_categories("CUSTOM_NODES", node_categories)
 
 def unregister():
     nodeitems_utils.unregister_node_categories("CUSTOM_NODES")
     spectrum.unregister()
     intensity.unregister()
+    del bpy.types.Scene.prism_props_import_files
     bpy.utils.unregister_module(__name__)
